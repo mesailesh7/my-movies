5d0df23b07b5a659b4180163f7f6da80ecd86e54
Sonu Thapa <37073011+mesailesh7@users.noreply.github.com>
2025-02-05 11:13:49 -0700

    Update authMiddleware.js
    Bug Update

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -26,7 +26,7 @@ const authenticate = asyncHandler(async (req, res, next) => {
 
 //check if the user is Admin or not
 const authorizeAdmin = (req, res, next) => {
-  if (req.user || req.user.isAdmin) {
+  if (req.user && req.user.isAdmin) {
     next();
   } else {
     res.status(401).send("not authorized as an admin");
